d72294283ac1949a00103bb7a7c4c4deb5969b55
chenyongchang <www.1759633997@qq.com>
2022-02-09 13:34:21 +0800

    1

--- js/index.js
+++ js/index.js
@@ -251,24 +251,24 @@
   index.renderPage();
   // 一开始的轮播图
   // index.initSwiper();
-  index.initSwiper2();
-  w.changeIndex = (i) => {
-    if (index.indexId !== i) {
-      index.indexId = i;
-      // index.initSwiper();
-      index.initSwiper2();
-      return false;
-    }
-  };
+  // index.initSwiper2();
+  // w.changeIndex = (i) => {
+  //   if (index.indexId !== i) {
+  //     index.indexId = i;
+  //     // index.initSwiper();
+  //     // index.initSwiper2();
+  //     return false;
+  //   }
+  // };
   w.videoStatusChang = (flag) => {
     const video = d.querySelector(".video");
     console.log(video.currentTime);
     //false 点击了播放  true 点击了暂停
     console.log(video.paused);
-    if(video.paused){
-      video.play()
-    }else{
-      video.pause()
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
     }
   };
   const allScrollNumber = index.getAllScrollNumber();
@@ -298,4 +298,91 @@
     index.handleScroll();
   });
   // index.numberScroll({className: 'dd-num-scroll'});
+  lock = false;
+  // bgColor = ["rgb(179, 189, 196)","rgb(180, 183, 166)","rgb(140, 152, 187)"]; //背景色
+  var swiper1 = new Swiper(".swiper1", {
+    speed: 1300,
+    allowTouchMove: false, //禁止触摸滑动
+    // parallax : true,  //文字位移视差
+    init: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next1",
+      prevEl: ".swiper-button-prev1",
+    },
+    autoplay: {
+      delay: 4500,
+      stopOnLastSlide: false,
+      // 当设置为false时，用户操作之后autoplay不会被禁掉
+      disableOnInteraction: false,
+    },
+    on: {
+      transitionStart: function () {
+        // if ($(window).width() > 768) {
+        lock = true; //锁住按钮
+        slides = this.slides;
+        imgBox = slides.eq(this.previousIndex).find(".pc"); //图片包装器
+        imgPrev = slides.eq(this.previousIndex).find(".pc img"); //当前图片
+        imgActive = slides.eq(this.activeIndex).find(".pc img"); //下一张图片
+        derection = this.activeIndex - this.previousIndex;
+        // this.$el.css("background-color",bgColor[this.activeIndex]);//背景颜色动画
+        imgBox.transform("matrix(0.95, 0, 0, 0.95, 0, 0)");
+        imgPrev.transition(1000).transform("matrix(1.1, 0, 0, 1.1, 0, 0)"); //图片缩放视差
+        this.slides
+          .eq(this.previousIndex)
+          .find("h3")
+          .transition(1000)
+          .css("color", "rgba(255,255,255,0)"); //文字透明动画
+        imgPrev.transitionEnd(function () {
+          imgActive
+            .transition(1300)
+            .transform("translate3d(0, 0, 0) matrix(1.1, 0, 0, 1.1, 0, 0)"); //图片位移视差
+          imgPrev
+            .transition(1300)
+            .transform(
+              "translate3d(" +
+                60 * derection +
+                "%, 0, 0)  matrix(1.1, 0, 0, 1.1, 0, 0)"
+            );
+        });
+        // }
+      },
+      transitionEnd: function () {
+        // if ($(window).width() > 768) {
+        this.slides
+          .eq(this.activeIndex)
+          .find(".pc")
+          .transform(" matrix(1, 0, 0, 1, 0, 0)");
+        imgActive = this.slides.eq(this.activeIndex).find("img");
+        imgActive.transition(1000).transform(" matrix(1, 0, 0, 1, 0, 0)");
+        this.slides
+          .eq(this.activeIndex)
+          .find("h3")
+          .transition(1000)
+          .css("color", "rgba(255,255,255,1)");
+        // }
+      },
+      init: function () {
+        this.emit("transitionEnd"); //在初始化时触发一次transitionEnd事件
+        swiperAnimate(this); //初始化完成开始动画
+      },
+      slideChangeTransitionEnd: function () {
+        swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+      },
+      slideChangeTransitionStart: function () {
+        var index = this.activeIndex;
+      },
+    },
+    breakpoints: {
+      768: {
+        allowTouchMove: true, //禁止触摸滑动
+        // loop: true
+      },
+    },
+  });
+  // $(".swiper1").show();
+  swiper1.init();
 })(window, document);
